9f5da81f3f806636d2034879df88d1b82cf77b89
salvador lopez <li.salvadorlopez@gmail.com>
2024-10-26 15:34:47 -0600

    Added titles service

---
+++ titles-service/src/main/java/in/juulk/titlesservice/TitlesServiceApplication.java
@@ -0,0 +1,13 @@
+package in.juulk.titlesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitlesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitlesServiceApplication.class, args);
+    }
+
+}
---
+++ titles-service/src/test/java/in/juulk/titlesservice/TitlesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.juulk.titlesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitlesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
