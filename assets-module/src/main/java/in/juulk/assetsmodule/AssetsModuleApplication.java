109857c7f84fccd87a96974c0a56e7e2a1990170
salvador lopez <li.salvadorlopez@gmail.com>
2024-10-26 15:30:15 -0600

    Added assets module

---
+++ assets-module/src/main/java/in/juulk/assetsmodule/AssetsModuleApplication.java
@@ -0,0 +1,13 @@
+package in.juulk.assetsmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetsModuleApplication.class, args);
+    }
+
+}
---
+++ assets-module/src/test/java/in/juulk/assetsmodule/AssetsModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.juulk.assetsmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
