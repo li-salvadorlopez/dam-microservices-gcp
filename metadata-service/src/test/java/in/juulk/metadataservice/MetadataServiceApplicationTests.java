985fdc9d8c2d07fdbba597682b934c9aa0fe1bb4
salvador lopez <li.salvadorlopez@gmail.com>
2024-10-26 15:33:01 -0600

    Added metadata service

---
+++ metadata-service/src/test/java/in/juulk/metadataservice/MetadataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.juulk.metadataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
